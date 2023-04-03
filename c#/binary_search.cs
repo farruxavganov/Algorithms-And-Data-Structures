82f458b6718afed89bde8cf6c1b0ac80b06cb88b
farruxavganov <farrux.avganov@mail.ru>
2023-04-03 15:30:34 -0400

    added some foldirs and files for binary search in c, c#, js

---
+++ c#/binary_search.cs
@@ -0,0 +1,9 @@
+using System
+
+namespace BinarySearch {
+	class Program {
+		static void Main(){
+			System.Console.WriteLine("Binary search");
+		}
+	}
+}
---
+++ c/binary_search.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("%s", "binary search");
+	return 0;
+}
