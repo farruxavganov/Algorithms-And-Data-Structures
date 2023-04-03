f972448a218e42cf7735368d17f578c627031d1b
farruxavganov <farrux.avganov@mail.ru>
2023-04-03 15:19:34 -0400

    finished binary search algorithm in python

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,1 +1,19 @@
-pass
+#it should return target index if target in arr
+def binary_search(arr: list, target: int) -> int:
+	"""Binary search algorithm for int values"""
+	left = 0
+	right = len(arr) - 1
+	
+	while left <= right:
+		mid = (left + right) // 2
+		guess = arr[mid]
+		
+		if guess == target:
+			return mid
+		if guess <= target:
+			left = mid + 1
+			continue
+		right = mid - 1
+		
+	return -1 #if value none in arr
+
