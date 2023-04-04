05c863d7593aa21fc0fb31cbb56516fd91dff4d8
farruxavganov <farrux.avganov@mail.ru>
2023-04-04 11:41:34 -0400

    finished binary search algorithm in js

--- javascript/binary_search.js
+++ javascript/binary_search.js
@@ -1,1 +1,22 @@
-console.log('binary search');
+//it should return target index
+function binarySearch(arr, target) {
+	let left = 0;
+	let right = arr.length - 1;
+
+	while(left <= right){
+		let mid = Math.floor((left + right) / 2);
+		let guess = arr[mid];
+
+		if(guess === target)
+			return mid
+
+		if(guess < target){
+			left = mid + 1;
+			continue
+		}
+
+		right = mid - 1;
+	}
+
+	return -1
+}
