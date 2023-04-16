221a95cace34386d076a3c924b1a0bdb85422ba5
farruxavganov <farrux.avganov@mail.ru>
2023-04-16 15:06:23 -0400

    fineshed next selection sort algo in js

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -22,3 +22,29 @@ function selectionSort(nums) {
 		nums[i] = temp;
 	}
 }
+
+
+//next example
+
+function findIndex(nums) {
+	let minIndex = 0;
+
+	for(let i = 0; i < nums.length; i++){
+		if(nums[i] < nums[minIndex]){
+			minIndex = i
+		}
+	}
+
+	return minIndex;
+}
+
+function swapIndex(nums) {
+	let length = nums.length;
+	let newNums = []
+	for(let i = 0; i < length; i++){
+		let minIndex = findIndex(nums);
+		newNums.push(nums.splice(minIndex, 1)[0]);
+	}
+
+	return newNums;
+}
