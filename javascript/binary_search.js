3d24afb3d65f5dfe0b9669d31d753a35b3532ee9
farruxavganov <farrux.avganov@mail.ru>
2023-04-19 15:09:06 -0400

    added some ; in js

--- javascript/binary_search.js
+++ javascript/binary_search.js
@@ -8,15 +8,15 @@ function binarySearch(arr, target) {
 		let guess = arr[mid];
 
 		if(guess === target)
-			return mid
+			return mid;
 
 		if(guess < target){
 			left = mid + 1;
-			continue
+			continue;
 		}
 
 		right = mid - 1;
 	}
 
-	return -1
+	return -1;
 }
